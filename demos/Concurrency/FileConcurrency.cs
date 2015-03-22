cd121a3bef7bf78df4241c7f35e55c3bbfe2bb47
Jeffrey Zhang <psn.jeffreyzhang@hotmail.com>
2015-03-22 22:58:55 +0800

    file concurrency

---
+++ demos/Concurrency/FileConcurrency.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace demos.Concurrency
+{
+    public static class FileConcurrency
+    {
+        public static string OpenFile(string path, int threadCount)
+        {
+            var result = new StringBuilder();
+            var results = new string[threadCount];
+            var finished = 0;
+            var succeed = 0;
+
+            var start = false; // to control threads
+            for (var i = 0; i < threadCount; i++)
+            {
+                var i1 = i;
+                var thread = new Thread(new ThreadStart(delegate
+                {
+                    while (!start) Thread.Sleep(1);
+
+                    FileStream fs = null;
+                    var fileResult = "";
+                    try
+                    {
+                        using (fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                        {
+                            var buff = new byte[fs.Length];
+                            fs.Read(buff, 0, buff.Length);
+                            fileResult += "size: " + buff.Length;
+                            succeed++;
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        fileResult += "err: " + e.Message.Substring(0, 6) + "...";
+                    }
+                    finally
+                    {
+                        finished++;
+                        if (fs != null) fs.Close();
+                    }
+
+                    // print thread infomation and result.
+                    results[i1] = string.Format("time: {0}, num: {1:D3}, id: {2:D3}, {3}\r\n",
+                        DateTime.Now.ToString("mm-ss.fff"),
+                        i1,
+                        Thread.CurrentThread.ManagedThreadId,
+                        fileResult
+                        );
+                }));
+                thread.Start();
+            }
+            Thread.Sleep(200);
+            start = true; // all threads start to read file.
+
+// ReSharper disable LoopVariableIsNeverChangedInsideLoop
+            while (finished < threadCount)
+// ReSharper restore LoopVariableIsNeverChangedInsideLoop
+            {
+                Thread.Sleep(1);
+            }
+            foreach (var s in results)
+            {
+                result.Append(s);
+            }
+
+            result.Append(succeed + "/" + threadCount + " succeeded, ratio: " + ((double) succeed/threadCount)*100 + "%");
+            return result.ToString();
+        }
+    }
+}
