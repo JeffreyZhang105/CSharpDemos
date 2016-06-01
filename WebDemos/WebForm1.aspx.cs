0be326e18481f97c8a6aaaf4b866a69e46ebc907
張 景富 <zhangjf@hyron-js.com>
2016-06-01 17:38:43 +0800

    MVC project added

---
+++ WebDemos/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebDemos
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var a = 1;
+            var b = 2;
+            Response.Redirect("http://www.baidu.com");
+        }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            var test = 1;
+            var test2 = 2;
+        }
+    }
+}
---
+++ demos/Temp/LunaLogAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demos.Temp
+{
+    public class LunaLogAnalyzer
+    {
+        string accessLogPath8 = @"E:\Downloads\20160328_aupass_log2\10.24.23.8\httpd\access.lnln.2016032423.log";
+        string accessLogPath9 = @"E:\Downloads\20160328_aupass_log2\10.24.23.9\httpd\access.lnln.2016032423.log";
+        string cataLogPath8 = @"E:\Downloads\20160328_aupass_tomcat0325\8\catalina.out-20160325";
+        string cataLogPath9 = @"E:\Downloads\20160328_aupass_tomcat0325\9\catalina.out-20160325";
+        string resultPath = @"E:\20160328\result.log";
+
+        public void AnalyzeLogs()
+        {
+            using (var access8 = new StringReader(accessLogPath8))
+            using (var access9 = new StringReader(accessLogPath9))
+            using (var cata8 = new StringReader(cataLogPath8))
+            using (var cata9 = new StringReader(cataLogPath9))
+            {
+                string line8, line9;
+                while (!string.IsNullOrWhiteSpace(line8 = access8.ReadLine()))
+                {
+
+                }
+            }
+        }
+    }
+}
