4b5b38678d0dec376bc3c1af10e66838836c0516
張 景富 <zhangjf@hyron-js.com>
2016-07-29 09:42:40 +0800

    no message

---
+++ demos/StreamStudy/MyStream.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demos.StreamStudy
+{
+    public class MyStream:Stream
+    {
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead { get; }
+        public override bool CanSeek { get; }
+        public override bool CanWrite { get; }
+        public override long Length { get; }
+        public override long Position { get; set; }
+
+        public void test()
+        {
+            var ms=new MemoryStream();
+            ms.Write(buffer: new byte[](),offset: 0,count: 0);
+        }
+    }
+}
