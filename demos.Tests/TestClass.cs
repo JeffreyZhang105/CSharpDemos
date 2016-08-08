2130e38b32852a70eb1ed8c71a3ffad5c41a1e4d
張 景富 <zhangjf@hyron-js.com>
2016-08-08 11:07:15 +0800

    NUnit project added; Test classes for asynchronism added.

---
+++ demos.Tests/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using demos.Asynchronism;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demos.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var target = new AsyncAndAwait();
+
+            var result = target.TestAsync();
+
+            Assert.Pass();
+        }
+    }
+}
