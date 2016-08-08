c5c8ad9811f6fc1a47edb755e656d8e17a285d94
張 景富 <zhangjf@hyron-js.com>
2016-08-08 11:06:17 +0800

    Async testing added.

---
+++ demos/Asynchronism/AsyncAndAwait.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demos.Asynchronism
+{
+    public class AsyncAndAwait
+    {
+        public async static Task<string> GetStringAsync()
+        {
+            var result = string.Empty;
+
+            Thread.Sleep(10000);
+
+            result = Guid.NewGuid().ToString();
+            return result;
+        }
+
+        public string TestAsync()
+        {
+            var result = string.Empty;
+
+            var task = GetStringAsync();
+            var awaiter = task.GetAwaiter();
+            result = awaiter.GetResult();
+
+            return result;
+        }
+    }
+}
